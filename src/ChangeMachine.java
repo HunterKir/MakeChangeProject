3105e9d8d847ea70a8b8eb83763833c418ca9008
HunterKir <hunter.kiritsis@gmail.com>
2017-09-29 20:02:14 -0600

    added field array, started change method

--- src/ChangeMachine.java
+++ src/ChangeMachine.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
-
+// 100, 50, 20, 10, 5, 1, .25, .10, .05, .01
 public class ChangeMachine {
-
+	static int change[] = new int[10];
 	public static void main(String[] args) {
 		Scanner kb = new Scanner(System.in);
 		double price, payment;
@@ -15,6 +15,13 @@ public class ChangeMachine {
 		if (price > payment) {
 			System.out.println("That is not enough money!");
 		}
+		if (price == payment) {
+			System.out.println("You've given the exact amount and don't need any change!");
+		}
+	}
+	
+	public static void makeChange(double payment) {
+		
 	}
 
 }
