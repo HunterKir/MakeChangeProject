b4f3ae3dda2dd778d3ba57ef975bb5c9c6ee6df9
HunterKir <hunter.kiritsis@gmail.com>
2017-09-29 21:07:09 -0600

    change machine has full functionality

--- src/ChangeMachine.java
+++ src/ChangeMachine.java
@@ -4,13 +4,13 @@ public class ChangeMachine {
 	static int change[] = new int[10];
 	public static void main(String[] args) {
 		Scanner kb = new Scanner(System.in);
-		double price, payment, difference;
-		int differenceInt;
+		double price, payment;
+		int difference;
 		
 		System.out.print("Enter the price of your item: ");
 		price = kb.nextDouble();
 		
-		System.out.print("How much money will you give the cashier? : ");
+		System.out.print("How much money will you give the machine? : ");
 		payment = kb.nextDouble();
 		
 		if (price > payment) {
@@ -20,12 +20,12 @@ public class ChangeMachine {
 			System.out.println("You've given the exact amount and don't need any change!");
 		}
 		else {
-			difference = payment - price;
-			differenceInt = (int)(difference * 100);
-			System.out.println(differenceInt);
-			makeChange(differenceInt);
+			difference = (int)((payment - price) * 100);
+			makeChange(difference);
 		}
+		kb.close();
 		
+		System.out.println("Your change:");
 		for (int i = 0; i < change.length; i++) {
 			if (change[i] > 0) {
 				do {
@@ -49,58 +49,57 @@ public class ChangeMachine {
 		else if (arrIndex == 5)
 			System.out.print("$1 ");
 		else if (arrIndex == 6)
-			System.out.print(".25c ");
+			System.out.print("25\u00A2 ");
 		else if (arrIndex == 7)
-			System.out.print(".10c ");
+			System.out.print("10\u00A2 ");
 		else if (arrIndex == 8)
-			System.out.print(".05c ");
+			System.out.print("5\u00A2 ");
 		else if (arrIndex == 9)
-			System.out.print(".01c ");
+			System.out.print("1\u00A2 ");
 	}
 	
-	public static void makeChange(int differenceInt) {
-		while (differenceInt != 0) {
-			if (differenceInt - 10000 >= 0) {
+	public static void makeChange(int difference) {
+		while (difference != 0) {
+			if (difference - 10000 >= 0) {
 				change[0]++;
-				differenceInt -= 10000;
+				difference -= 10000;
 			}
-			else if (differenceInt - 5000 >= 0) {
+			else if (difference - 5000 >= 0) {
 				change[1]++;
-				differenceInt -= 5000;
+				difference -= 5000;
 			}
-			else if (differenceInt - 2000 >= 0) {
+			else if (difference - 2000 >= 0) {
 				change[2]++;
-				differenceInt -= 2000;
+				difference -= 2000;
 			}
-			else if (differenceInt - 1000 >= 0) {
+			else if (difference - 1000 >= 0) {
 				change[3]++;
-				differenceInt -= 1000;
+				difference -= 1000;
 			}
-			else if (differenceInt - 500 >= 0) {
+			else if (difference - 500 >= 0) {
 				change[4]++;
-				differenceInt -= 500;
+				difference -= 500;
 			}
-			else if (differenceInt - 100 >= 0) {
+			else if (difference - 100 >= 0) {
 				change[5]++;
-				differenceInt -= 100;
+				difference -= 100;
 			}
-			else if (differenceInt - 25 >= 0) {
+			else if (difference - 25 >= 0) {
 				change[6]++;
-				differenceInt -= 25;
+				difference -= 25;
 			}
-			else if (differenceInt - 10 >= 0) {
+			else if (difference - 10 >= 0) {
 				change[7]++;
-				differenceInt -= 10;
+				difference -= 10;
 			}
-			else if (differenceInt - 5 >= 0) {
+			else if (difference - 5 >= 0) {
 				change[8]++;
-				differenceInt -= 5;
+				difference -= 5;
 			}
 			else {
 				change[9]++;
-				differenceInt--;
+				difference--;
 			}
-				
 		}
 	}
 }
